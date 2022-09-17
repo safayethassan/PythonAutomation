39b60118486512fafb5012d1d634734edcd92b7e
Md. Safayet Hassan <safayethassan340@gmail.com>
2022-09-17 17:09:54 +0600

    Update voterInfoPage.py

--- voterInfoPage.py
+++ voterInfoPage.py
@@ -9,27 +9,27 @@ class voterInfoClass:
 #    webDriverDetails.driverClass.driver.get("https://prportal.nidw.gov.bd/nid-pub/voter-info")
 #    click_on_voter_info_button = webDriverDetails.driverClass.driver.find_element_by_css_selector("a[href='/nid-pub/voter-info']")
 
-    webDriverDetails.driverClass.driver.get("http://localhost:8080/nid-pub/voter-info")
+    webDriverDetails.driverClass.driver.get("dummy webpage")
 
-    set_nid = "3756747436"
+    set_nid = "dummy nid"
 
     set_nid_input_box = webDriverDetails.driverClass.driver.find_element_by_css_selector("input[name='nid']")
 
     set_nid_input_box.send_keys(set_nid)
 
-    set_day = "10"
+    set_day = "dummy"
 
     set_day_input_box = webDriverDetails.driverClass.driver.find_element_by_css_selector("input[name='day']")
 
     set_day_input_box.send_keys(set_day)
 
-    set_month = "12"
+    set_month = "dummy"
 
     set_month_input_box = webDriverDetails.driverClass.driver.find_element_by_css_selector("input[name='month']")
 
     set_month_input_box.send_keys(set_month)
 
-    set_year = "1995"
+    set_year = "dummy"
 
     set_year_input_box = webDriverDetails.driverClass.driver.find_element_by_css_selector("input[name='year']")
 
